443797621e6b5b4ca36d3d5fbc602de74ef9b0ec
Edgardo Marchi <edgardo.marchi@gmail.com>
2019-05-08 11:51:17 -0300

    Working project tcl

--- memmv.vhd
+++ memmv.vhd
@@ -1,37 +1,49 @@
 ------------------------------------------------------------------------------------
--- memmv
+--* memmv.vhd
+--* @brief A module that executes a UDMA instruction for data moving within a system.
 --
 --
---
--- Autor: E. Marchi - M. Cervetto
--- Revisión: 0.1 -- inicial
+--* @autor E. Marchi, M. Cervetto
+--* @version 0.1 -- Initial
 -------------------------------------------------------------------------------------
 
 library IEEE;
+library work;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use work.memmv_params.all;
+
 
 entity memmv is
       port (
         clk : in std_logic;
         rst : in std_logic;
 
-        -- commblock side
+        -- Slave side
+        --* Start parsing command when setted
         start     : in  std_logic;
+        --* Command data input
         cmd_in    : in  std_logic_vector(DATA_WIDTH - 1 downto 0);
+        --* Address from where the command is read
         cmd_addr  : out std_logic_vector(ADDR_WIDTH - 1 downto 0);
+        --* Outs '1' in order to read command
         cmd_fetch : out std_logic;
 
-        -- fabric side
+        -- Master side
+        --* Write enable input for peripheral
         wr_en   : in  std_logic;
+        --* Data input for peripheral
         din     : in  std_logic_vector(DATA_WIDTH - 1 downto 0);
+        --* Data valid for peripheral
         dv      : out std_logic;
+        --* Data output for peipheral
         dout    : out std_logic_vector(DATA_WIDTH - 1 downto 0);
+        --* Address output for reading/writing data from/to peripheral
         address : out std_logic_vector(ADDR_WIDTH - 1 downto 0);
+        --* Read request output
         rd_rq   : out std_logic;
 
-        -- Status flags
+        --+ Status flags
         busy        : out std_logic;
         done        : out std_logic
 
